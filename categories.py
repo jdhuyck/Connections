fdf075d859ffe0be4d02d5f8688257cd4924a2db
Jody Huyck-Jones <jodyhj@outlook.com>
2025-02-21 12:48:44 +0000

    Added play.py with pre-made categories

---
+++ categories.py
@@ -0,0 +1,6 @@
+all_categories = {
+    "Planets": ["Mercury", "Venus", "Mars", "Saturn"],
+    "Metals": ["Copper", "Iron", "Silver", "Gold"],
+    "Chess Pieces": ["Knight", "Queen", "Rook", "King"],
+    "Fruits": ["Apple", "Orange", "Cherry", "Banana"]
+}
